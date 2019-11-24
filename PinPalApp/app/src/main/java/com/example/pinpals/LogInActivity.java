1f192b9e3e3a070d5b0c3c2c9504deeaa031668a
Mariah Gan <qimeng.gan@ucdconnect.ie>
2019-11-24 13:47:03 +0000

    Update LogInActivity.java

--- PinPalApp/app/src/main/java/com/example/pinpals/LogInActivity.java
+++ PinPalApp/app/src/main/java/com/example/pinpals/LogInActivity.java
@@ -36,68 +36,68 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(LogInActivity.this, MapsActivity.class);
-        intent.putExtra("time", "1234");
-        intent.putExtra("longitude", -6.266155);
-        intent.putExtra("latitude",  53.350140);
-        startActivity(intent);
+        OkHttpClient client = new OkHttpClient();
+        FormBody.Builder formBuilder = new FormBody.Builder();
+        formBuilder.add("user_id", Integer.toString(1));
+        Request request = new Request.Builder().url("http://10.0.2.2:5000/latest").post(formBuilder.build()).build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LogInActivity.this, "Fail to connect the server", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
 
-        Log.i("response", "???");
-//        OkHttpClient client = new OkHttpClient();
-//        FormBody.Builder formBuilder = new FormBody.Builder();
-//        formBuilder.add("user_id", Integer.toString(1));
-//        Request request = new Request.Builder().url("http://10.0.2.2:5000/latest").post(formBuilder.build()).build();
-//        Call call = client.newCall(request);
-//        call.enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                Toast.makeText(LogInActivity.this, "Fail to connect the server", Toast.LENGTH_SHORT).show();
-//                            }
-//                        });
-//                    }
-//                });
-//            }
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String res = response.body().string();
+                Log.i("response", res);
+                final HashMap<Integer, PinPal> PinPals = new HashMap<Integer, PinPal>();
+                try {
+
+                    JSONArray jsonArray = new JSONArray(res);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        if (jsonObject != null) {
+                            int reg = jsonObject.optInt("reg_code");
+                            String time = jsonObject.optString("time");
+                            double lat = jsonObject.optDouble("latitude");
+                            double lng = jsonObject.optDouble("longitude");
+                            // Package PinPal Object
+                            PinPal pinPal = new PinPal(reg, time, lat, lng);
+                            PinPals.put(pinPal.getReg(), pinPal);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("Hiiiiiiiii", PinPals.get(123456).getReg() + "");
+                        Intent intent = new Intent(LogInActivity.this, MapsActivity.class);
+                        intent.putExtra("reg", PinPals.get(123456).getReg());
+                        intent.putExtra("time", PinPals.get(123456).getTime());
+                        intent.putExtra("longitude", PinPals.get(123456).getLocation()[1]);
+                        intent.putExtra("latitude",  PinPals.get(123456).getLocation()[0]);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+        });
 //
-//            @Override
-//            public void onResponse(Call call, final Response response) throws IOException {
-//                final String res = response.body().string();
-//                Log.i("response", res);
-//                final HashMap<Integer, PinPal> PinPals = new HashMap<Integer, PinPal>();
-//                try {
-//
-//                    JSONArray jsonArray = new JSONArray(res);
-//
-//                    for (int i = 0; i < jsonArray.length(); i++) {
-//                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-//
-//                        if (jsonObject != null) {
-//                            int reg = jsonObject.optInt("reg_code");
-//                            String time = jsonObject.optString("time");
-//                            double lat = jsonObject.optDouble("latitude");
-//                            double lng = jsonObject.optDouble("longitude");
-//                            // Package PinPal Object
-//                            PinPal pinPal = new PinPal(reg, time, lat, lng);
-//                            PinPals.put(pinPal.getReg(), pinPal);
-//                        }
-//                    }
-//
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Log.i("Hiiiiiiiii",PinPals.get(123456).getReg()+"");
-//                    }
-//                });
-//
-//            }
-//        });
     }
 }
