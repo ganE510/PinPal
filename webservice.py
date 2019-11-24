b6dd90e9270b99c89124b66df6017c70e8678d9b
Mariah Gan <qimeng.gan@ucdconnect.ie>
2019-11-24 11:29:21 +0000

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, request
+from firebase_admin import messaging
 import json
 import DAO
 import datetime
@@ -20,8 +21,31 @@ def update_new():
     dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     gps = "aa234-nw456"
     DAO.update_latest(pin_id, dt, gps)
+
+
+def send_to_token():
+    # [START send_to_token]
+    # This registration token comes from the client FCM SDKs.
+    registration_token = 'YOUR_REGISTRATION_TOKEN'
+
+    # See documentation on defining a message payload.
+    message = messaging.Message(
+        data={
+            'score': '850',
+            'time': '2:45',
+        },
+        token=registration_token,
+    )
+
+    # Send a message to the device corresponding to the provided
+    # registration token.
+    response = messaging.send(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
+    # [END send_to_token]
     return redirect(url_for('resp_latest'))
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+
